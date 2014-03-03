6b173ec41e62b7101e481d15907c3d43275b3f99
Araki.Atsushi <senalove2007@gmail.com>
2014-03-04 08:38:51 +0900

    チャット機能の実装

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var MongoStore = require('connect-mongo')(express);
+var mongoose = require('mongoose');
+var socketio = require('socket.io');
 
 var app = express();
 
@@ -21,6 +23,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.bodyParser());
 app.use(express.cookieParser()); 
 app.use(express.session({
     secret: 'secret',
@@ -62,6 +65,45 @@ app.get('/logout', function(req, res){
   res.redirect('/');
 });
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app);
+// http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+// 待ち受けをSocket.IOに渡す
+var io = socketio.listen(server);
+
+//storechatというコレクションを使って（始めは作って）
+var db = mongoose.connect('mongodb://localhost/storechat')
+//スキーマ宣言
+var ChatSchema = new mongoose.Schema({
+  text:{type: String}
+  ,name:{type: String}
+  ,ipaddress:{type: String}
+});
+//スキーマからモデル生成
+var Chat = db.model('chat', ChatSchema);
+
+// クライアントの接続対応
+io.sockets.on('connection',function(socket){
+  //初めにチャットの全データをクライアントがもらう。
+  Chat.find(function(err, items){
+    if(err){console.log(err);}
+    //接続ユーザにDBのデータ送信
+    socket.emit('init', items);
+  });
+
+  // 個々のクライアントからの通信
+  socket.on('message',function(data){
+    // クライアントからもらってきたデータ（名前とメッセージ）を取得
+    var chat = new Chat(data);
+    // クライアントのアドレスを取得する
+    chat.ipaddress = socket.handshake.address.address
+      + ':' + socket.handshake.address.port;
+    chat.save(function(err){
+      if(err){return;}
+      socket.broadcast.emit('message',chat);
+    });
+  });
 });
